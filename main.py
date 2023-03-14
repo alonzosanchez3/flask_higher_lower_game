3bec38ad1ca4db01611fd7ecee062d6719136d5e
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-03-14 10:56:12 -0500

    added url guessing

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 from flask import Flask
 from dotenv import load_dotenv
 import os
+import random
 
 load_dotenv()
 
 app = Flask(__name__)
 
+random_number = random.randint(0,9)
+
 @app.route('/')
 def hello_world():
-  return '<h1 style="text-align: center">Hello, World!</h1>' \
-    '<p>This is a paragraph.</p>' \
+  return '<h1 style="text-align: center">Guess a number between 0 and 9</h1>' \
     '<img src="https://media.giphy.com/media/dX8PxV3YrA87tVFFdQ/giphy.gif">'
 
-@app.route('/bye')
-def bye():
-  return "Bye!"
+@app.route('/<int:guess>')
+def guess_number(guess):
+    if guess > random_number:
+        return "<h1 style='color: purple'>Too high, try again!</h1>" \
+               "<img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif'/>"
 
-@app.route('/username/<name>')
-def greet(name):
-  return f'Hello there, {name}'
+    elif guess < random_number:
+        return "<h1 style='color: red'>Too low, try again!</h1>"\
+               "<img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif'/>"
+    else:
+        return "<h1 style='color: green'>You found me!</h1>" \
+               "<img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'/>"
+
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0', port=4000)
