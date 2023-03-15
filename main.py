e5a1d356de04efd2f34b9cc2c9c61e3c6989208b
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-03-15 10:56:12 -0500

    added gifs

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+  return '<h1 style="text-align: center">Hello, World!</h1>' \
+    '<p>This is a paragraph.</p>' \
+    '<img src="https://media.giphy.com/media/dX8PxV3YrA87tVFFdQ/giphy.gif">'
 
 @app.route('/bye')
 def bye():
@@ -16,7 +18,7 @@ def bye():
 
 @app.route('/username/<name>')
 def greet(name):
-  return f'Hello, {name}'
+  return f'Hello there, {name}'
 
 if __name__ == '__main__':
-  app.run()
+  app.run(debug=True, host='0.0.0.0', port=4000)
